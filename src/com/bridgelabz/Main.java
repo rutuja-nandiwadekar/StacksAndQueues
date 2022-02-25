ec426eb9164ba296bc89e78c48a22f3a541c8820
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-25 23:15:30 +0530

    Added: Ability to create a Stack & pop element logic

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,18 +1,22 @@
 /*
- *Ability to create a Stack of 56->30->70
- *So 70 will be added first then 30 and then 56 to make 56 on top of the Stack
+ *Ability to peak and pop from the Stack till it is empty 56 ->30 ->70
  */
 package com.bridgelabz;
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stacks and Queues Problems");
 		Stack stack = new Stack();
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
+		stack.showElements();
 
+		stack.pop();
+		stack.showElements();
+		stack.pop();
+		stack.showElements();
+		stack.pop();
 		stack.showElements();
 	}
 }
